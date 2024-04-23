6fdacde4060af7cbfcf7bf686920fe694ddaf905
chocopanya <167622587+chocopanya@users.noreply.github.com>
2024-04-23 23:09:29 +0300

    Update Form3.cs
    updating

--- Form3.cs
+++ Form3.cs
@@ -15,10 +15,33 @@ namespace Koleso
 {
     public partial class Form3 : System.Windows.Forms.Form
     {
-
         public Form3()
         {
             InitializeComponent();
+            //var connection = new SqliteConnection("Data Source=Колесо.db");
+            //connection.Open();
+            //using (var connection = new SqliteConnection("Data Source=Колесо.db"))
+            //{
+            //    connection.Open();
+            //}
+
+        }
+
+        
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void Save_Click(object sender, EventArgs e)
@@ -41,17 +64,29 @@ namespace Koleso
                     string sqlExpression = "INSERT INTO clients (SNL, PhoneNumb, Car, CarNumb, Region, Break) VALUES (@FullName, @NumbPhone, @BrendCar, @NumbCar, @RegionCar, @TypeBreak)";
                     using (SqliteCommand command = new SqliteCommand(sqlExpression, connection))
                     {
+                        // Добавьте параметры
                         command.Parameters.AddWithValue("@FullName", SNL.Text);
                         command.Parameters.AddWithValue("@NumbPhone", PhoneNumb.Text);
                         command.Parameters.AddWithValue("@BrendCar", Car.Text);
                         command.Parameters.AddWithValue("@NumbCar", CarNumb.Text);
                         command.Parameters.AddWithValue("@RegionCar", Region.Text);
                         command.Parameters.AddWithValue("@TypeBreak", Break.Text);
+                        
+                        // Выполните запрос
                         command.ExecuteNonQuery();
                     }
-                   
+                    //string sqlExpression = "SELECT * FROM clients";
+                    //SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                    //using (SqliteDataReader reader = command.ExecuteReader())
+                    //{
+                    //    if (reader.HasRows) // если есть данные
+                    //    {
+                    //        while (reader.Read())   // построчно считываем данные
+                    //        {
+                    //        }
+                    //    }
+                    //}
                 }
-
                 MessageBox.Show("Пользователь добавлен", "Успешно", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
                 SNL.Text = null;
                 PhoneNumb.Text = null;
@@ -60,6 +95,9 @@ namespace Koleso
                 Region.Text = null;
                 Break.Text = null;
             }
+        }
+        private void SNL_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
